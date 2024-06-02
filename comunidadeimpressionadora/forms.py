dbc3980be6f69ae5713b8fedf16e8dd80e99c7d4
Marcelofdea <146047815+Marcelofdea@users.noreply.github.com>
2024-06-02 15:14:15 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/forms.py
@@ -0,0 +1,51 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comunidadeimpressionadora.models import Usuario
+from flask_login import current_user
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
+
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de perfil', validators=[FileAllowed(['jpg', 'png'])])
+    curso_excel = BooleanField('Excel Expert')
+    curso_powerbi = BooleanField('Power BI Expert')
+    curso_power_query = BooleanField('Power Query Expert')
+    curso_vba = BooleanField('VBA Expert')
+    #curso_Python = BooleanField('Python Expert')
+    #curso_ppt = BooleanField('Apresentações Expert')
+    #curso_sql = BooleanField('SQL Expert')
+    botao_submit_editarperfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com este E-mail. Cadastre outro E-mail.')
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do Post', validators=[DataRequired(), Length(2, 70)])
+    corpo = TextAreaField('Escreva seu Post aqui', validators=[DataRequired()])
+    botao_submit = SubmitField('Criar Post')
+
